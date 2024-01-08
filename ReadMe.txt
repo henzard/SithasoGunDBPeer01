5ff2f0e0d112ada35769f25839191ed6469810c0
Mashy <mbanga.anele@gmail.com>
2024-01-08 21:00:14 +0200

    Version 1.01

--- ReadMe.txt
+++
@@ -1,6 +0,0 @@
-1. create a folder e.g gun-peer-relay
-2. cd to this folder in powershell
-3. execute 'npm install express gun'
-4. create index.js file
-5. in start.bat, we have code to start the peer relay.
-6. you can open the (empty) URL at http://localhost:8000/
--- src/index.js
+++ src/index.js
@@ -2,18 +2,21 @@ const compression = require('compression')
 const express = require('express')
 const Gun = require('gun')
 
+//create an express application
 const app = express()
-const port = 8765
+//get port from environment or use 8765
+const port = process.env.PORT || 8765
 app.use(compression())
 app.use(Gun.serve)
 app.use(express.static(__dirname));
 
+//listen to a port for an incoming request
 const server = app.listen(port, () => {
-    console.log("Listening at: http://localhost:" + port)
+    console.log(`GunDB Relay Peer Listening on: http://localhost:${port}`)
 })
 
 app.get('/', (req, res) => {
-    res.send('Running on port: ' + port)
+    res.send('GunDB Relay Peer Running on port: ' + port)
 })
 
 Gun({ web: server })
