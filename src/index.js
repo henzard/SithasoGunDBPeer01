86ed789534f981e96406e7447d09c1f7bfdb30b6
Mashy <mbanga.anele@gmail.com>
2024-01-08 21:11:26 +0200

    Version 1.01

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const server = app.listen(port, () => {
 })
 
 app.get('/', (req, res) => {
-    res.send('GunDB Relay Peer Running on port: ' + port)
+    res.send(`GunDB Relay Peer Running on port: ${port}`)
 })
 
 Gun({ web: server })
